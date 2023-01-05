db675c24dcafdf21ff0a4935d24855e96df34b7a
Justus B <justus.c.brown@gmail.com>
2023-01-05 14:08:20 -0800

    broken

--- db/connection.js
+++ db/connection.js
@@ -1,35 +1,12 @@
 const { MongoClient } = require("mongodb");
-// Replace the uri string with your connection string.
 
-/**
- * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
- * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-*/
+const uri = `mongodb://127.0.0.1:27017/`;
 
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
-async function main() {
-    // we'll add code here soon
-    const uri = `mongodb://127.0.0.1:27017/`;
-    
-    const client = new MongoClient(uri, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    async function listDatabases(client){
-        databasesList = await client.db().admin().listDatabases();
-     
-        console.log("Databases:");
-        databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-    };
-    try {
-        await client.connect();
-        await listDatabases(client);
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-}
+let db = client.db("dating_db");
 
-
-main().catch(console.error);
+export { db };
---
+++ server.js
@@ -0,0 +1,14 @@
+import{ db} from "./db/connection";
+
+
+// const express = require("express");
+// require("dotenv").config();
+
+console.log(db)
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// const PORT = process.env.PORT;
